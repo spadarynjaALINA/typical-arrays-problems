37ddf3e906078570c00f687645be41d655cb5bca
Alina <alinagusarik@gmail.com>
2021-08-18 15:45:05 +0300

    fix: try fixed some bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array) || !array.length ) {
+        return 0;
+    }
+       
+    else {
+        return Math.min(... array);
+    }
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    else {
+        return Math.max.apply(null, array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || !array.length ) {
+        return 0;
+    }
+    return result = array.reduce((sum, current) => sum + current, 0) / array.length;
+    
+   
 }
